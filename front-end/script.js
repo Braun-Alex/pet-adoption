b00020c483c5d314613d78c0ba49e48af9f13c4c
artpod <rabotnikovv@gmail.com>
2023-10-05 15:55:01 +0300

    Simple button added

---
+++ front-end/script.js
@@ -0,0 +1,4 @@
+document.getElementById('testButton').addEventListener('click', function() {
+    // Тут ваш код для зв'язку з бекендом або будь-якої іншої дії
+    alert('Кнопка була натиснута!'); // Це просто приклад, можете замінити на свій код
+});
