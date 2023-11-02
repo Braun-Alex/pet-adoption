5716527637340be170de6ea378455f22cd52388d
Danylo Spitsyn <danil.spitsyn7@gmail.com>
2023-11-03 01:47:00 +0200

    backend works

--- back-end/services/userService/UserService.cpp
+++ back-end/services/userService/UserService.cpp
@@ -10,9 +10,11 @@ using namespace Poco::Util;
 using Poco::ActiveRecord::Context;
 
 
-UserService::UserService(/* args */): pUserController_(new UserController)
+UserService::UserService(/* args */): pUserController_(new UserController())
 {
-    std::cout<<": UserService constructor. pUserController_ == nullptr: " << (pUserController_) << "\n";
+    Application& app = Application::instance();
+    app.logger().information(": UserService constructor. pUserController_ ");
+   // std::cout<<": UserService constructor. pUserController_ == nullptr: " << (pUserController_) << "\n";
 }
 
 UserService::~UserService()
@@ -25,27 +27,47 @@ void UserService::registerUser(const HTTPServerRequest& request, HTTPServerRespo
     // app.logger().information("Request \"Sign up user\" from %s", clientAddress);
 
    // HTMLForm form(request, request.stream());
+   // HTMLForm form(request, request.stream());
+   // HTMLForm form(request, request.stream());
+   Application& app = Application::instance();
+   std::string userEmail = "aboba";
+    std::string userPassword = "aboba";
+
+        LocalStructs::User user = {userEmail, userPassword};
+
+    app.logger().information(": UserService::registerUser called.  userEmail: %s,  userPassword: %s", user.email, user.password);
     setHeaderResponse(response);
     response.setContentType("text/html");
 
+    response.setStatus(HTTPResponse::HTTP_OK);
+
     // auto userEmail = form.find("user-email");
     // auto userPassword = form.find("user-password");
-    std::string userEmail = "aboba";
-    std::string userPassword = "aboba";
 
-    LocalStructs::User user = {userEmail, userPassword};
 
     std::cout<<": UserService::registerUser called." << " userEmail: " << user.email<<" userPassword: "<< user.password<<"\n";
 
    
-    //pUserController_->registerUser(user);
+    pUserController_->registerUser(user);
+    response.send();
 }
 
 void UserService::authorizeUser(const HTTPServerRequest& request, HTTPServerResponse& response){
+    Application& app = Application::instance();
+   
+    app.logger().information(": UserService::registerUser called.");
+
+    response.setContentType("text/html");
+
+    response.setStatus(HTTPResponse::HTTP_OK);
+
+
     std::cout<<": UserService::authorizeUser called.";
     std::string userEmail = "aboba";
     std::string userPassword = "aboba";
     LocalStructs::User user= {userEmail, userPassword};
 
     pUserController_->authorizeUser(user);
+
+    response.send();
 }
