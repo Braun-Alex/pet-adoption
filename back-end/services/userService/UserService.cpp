7c6b7e5936cbe513b1b573a122d6b231f06cdef9
Danylo Spitsyn <danil.spitsyn7@gmail.com>
2023-11-02 22:53:47 +0200

    aboba

--- back-end/services/userService/UserService.cpp
+++ back-end/services/userService/UserService.cpp
@@ -24,25 +24,20 @@ void UserService::registerUser(const HTTPServerRequest& request, HTTPServerRespo
     // const std::string& clientAddress = request.clientAddress().toString();
     // app.logger().information("Request \"Sign up user\" from %s", clientAddress);
 
-    HTMLForm form(request, request.stream());
+   // HTMLForm form(request, request.stream());
     setHeaderResponse(response);
     response.setContentType("text/html");
 
-    auto userEmail = form.find("user-email");
-    auto userPassword = form.find("user-password");
-    // std::string userEmail = "aboba";
-    // std::string userPassword = "aboba";
+    // auto userEmail = form.find("user-email");
+    // auto userPassword = form.find("user-password");
+    std::string userEmail = "aboba";
+    std::string userPassword = "aboba";
 
     LocalStructs::User user = {userEmail, userPassword};
 
     std::cout<<": UserService::registerUser called." << " userEmail: " << user.email<<" userPassword: "<< user.password<<"\n";
 
-    if isUserExist(){
-        ....
-    }
-    else{
-        controller.createUser(user);
-    }
+   
     //pUserController_->registerUser(user);
 }
 
