f5d126cee07b7e246175d8be530b0a6ebc2c98cc
artpod <rabotnikovv@gmail.com>
2023-11-10 10:03:32 +0200

    authorization, registration done

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,6 @@
 // App.js
-import React from 'react';
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom';
 import Home from './Pages/Home';
 import Contacts from './Pages/Contacts';
 import Donate from './Pages/Donate';
@@ -14,15 +14,31 @@ import './css/Signup.css'
 import './css/ShelterAcc.css'
 
 function App() {
+  const navigate = useNavigate(); // Отримуємо функцію navigate
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [username, setUsername] = useState("");
+
+  const handleLoginSuccess = () => {
+    setIsAuthenticated(true);
+    navigate('/');
+  };
+
+  const handleLogout = () => {
+    setIsAuthenticated(false);
+    setUsername('');
+    navigate('/login'); // Редірект на сторінку входу
+  };
+
   return (
     
       <div>
-      <Header />
+      <Header isAuthenticated={isAuthenticated} handleLogout={handleLogout} username={username} />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/donate' element={<Donate />} />
         <Route path='/contacts' element={<Contacts />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/login' element={<Login onLoginSuccess={handleLoginSuccess} />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/shelter-account'  element={<ShelterAcc />} />
       </Routes>  
--- front-end/src/Components/Header.js
+++ front-end/src/Components/Header.js
@@ -1,36 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-var isAuthenticated = true;
-var username = "ім'я користувача";
 
-function Header({showButton}) {
+function Header({ isAuthenticated, username, handleLogout }) {
   return (
     <header>
       <div className="header-left">
-        <Link to="/"><a>Головна</a></Link>          
-        <Link to="/donate"><a>Допомогти</a></Link>
-        <Link to="/contacts"><a>Контакти</a></Link>
+        <Link to="/">Головна</Link>          
+        <Link to="/donate">Допомогти</Link>
+        <Link to="/contacts">Контакти</Link>
       </div>
       
       <div className="header-right">
         {isAuthenticated ? (
           <>
-            <span className="userName-header"> {username} </span>
-            <button className="logout-button"> Вийти </button>
+            <span className="userName-header">{username}</span>
+            <button className="logout-button" onClick={handleLogout}>Вийти</button>
           </>
         ) : (
           <>
-            <Link to="/login">
-              <a className="login-button">Увійти</a>
-            </Link>
-            <Link to="/signup">
-              <a className="signup-button">Зареєструватися</a>
-            </Link>
+            <Link to="/login" className="login-button">Увійти</Link>
+            <Link to="/signup" className="signup-button">Зареєструватися</Link>
           </>
         )}
       </div>
     </header>
-    
   );
 }
 
