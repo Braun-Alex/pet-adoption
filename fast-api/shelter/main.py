670deeb8dbe559aa1fc4373d064da86f0629f751
Danylo Spitsyn <danil.spitsyn7@gmail.com>
2023-11-19 15:25:15 +0200

    sheler service added to nginx config

--- fast-api/shelter/main.py
+++ fast-api/shelter/main.py
@@ -1,16 +1,13 @@
 import os
 
 from fastapi.middleware.cors import CORSMiddleware
-from database import Base, engine, SessionLocal
-from service.shelter_service import ShelterService
-from controllers.shelter_controller import ShelterController
-from models.shelter_local_model import ShelterLocal
-from fastapi import FastAPI, Depends
-from fastapi.security import OAuth2PasswordRequestForm
-from dependencies.dependencies import get_current_shelter
-from utilities.utilities import TokenSchema
 
-app = FastAPI()
+from fastapi import FastAPI
+
+
+from shelter import shelter_route
+
+app = FastAPI(openapi_url="/api/v1/shelter/openapi.json", docs_url="/api/v1/shelter/docs")
 
 app.add_middleware(
     CORSMiddleware,
@@ -20,28 +17,34 @@ app.add_middleware(
     allow_headers=["*"],  # Дозволяє всі заголовки
 )
 
-db = SessionLocal()
 
-shelter_service = ShelterService(shelter_controller=ShelterController(db=db))
-
-Base.metadata.create_all(bind=engine)
-
+app.include_router(shelter_route, prefix='/api/v1/shelter', tags=["shelter"])
 
 @app.get("/")
-def read_root():
-    return {"Hi from shelter": os.urandom(32).hex()}
+def biba():
+    return {"message": "Biba handler"}
+# db = SessionLocal()
+
+# shelter_service = ShelterService(shelter_controller=ShelterController(db=db))
+
+# Base.metadata.create_all(bind=engine)
 
 
-@app.post("/shelter/signup", response_model=bool)
-def register_user(shelter: ShelterLocal):
-    return shelter_service.register_shelter(shelter=shelter)
+# @app.get("/")
+# def read_root():
+#     return {"Hi from shelter": os.urandom(32).hex()}
 
 
-@app.post("/shelter/login", response_model=TokenSchema)
-def authorize_user(shelter: OAuth2PasswordRequestForm = Depends()):
-    return shelter_service.authorize_shelter(shelter=shelter)
+# @app.post("/shelter/signup", response_model=bool)
+# def register_user(shelter: ShelterLocal):
+#     return shelter_service.register_shelter(shelter=shelter)
 
 
-@app.get('/shelter/profile', response_model=str)
-def get_shelter(token_payload=Depends(get_current_shelter)):
-    return shelter_service.get_shelter(token_payload.sub)
+# @app.post("/shelter/login", response_model=TokenSchema)
+# def authorize_user(shelter: OAuth2PasswordRequestForm = Depends()):
+#     return shelter_service.authorize_shelter(shelter=shelter)
+
+
+# @app.get('/shelter/profile', response_model=str)
+# def get_shelter(token_payload=Depends(get_current_shelter)):
+#     return shelter_service.get_shelter(token_payload.sub)
