161a46a3eba13207605c849ec01d4d8576e8d112
Danylo Spitsyn <danil.spitsyn7@gmail.com>
2023-11-24 00:00:40 +0200

    updated Shelter struct and converter

--- fast-api/shelter/utilities/converter.py
+++ fast-api/shelter/utilities/converter.py
@@ -8,6 +8,7 @@ def convert_from_shelter_db_to_local(shelter_db: ShelterDB) -> ShelterLocalOutpu
                                 full_name=shelter_db.name if shelter_db.name is not None else "",
                                 email=shelter_db.email if shelter_db.email is not None else "",
                                 phone_number=shelter_db.number if shelter_db.number is not None else "",
-                                description=shelter_db.description if shelter_db.description is not None else ""
+                                description=shelter_db.description if shelter_db.description is not None else "",
+                                address=shelter_db.address if shelter_db.address is not None else ""
                             )
 
