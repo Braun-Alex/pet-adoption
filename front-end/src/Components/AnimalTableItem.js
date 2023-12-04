c366c295a01c9d240606a9353232feb1864c720a
artpod <75904655+artpod@users.noreply.github.com>
2023-12-04 09:52:52 +0200

    Reactive updating of the animal has been added (#40)
    Co-authored-by: Braun-Alex <lamanasakasa@gmail.com>

--- front-end/src/Components/AnimalTableItem.js
+++ front-end/src/Components/AnimalTableItem.js
@@ -11,11 +11,7 @@ class AnimalTableItem extends Component {
         super(props);
 
         this.state = {
-            photo: null,
-            showShelterAcc: true,
-            showEditAcc: false,
-            showRequestList: false,
-            showAnimal: false
+            showAnimal: true
         };
 
         this.handleSubmitApplication = this.handleSubmitApplication.bind(this);
@@ -26,42 +22,44 @@ class AnimalTableItem extends Component {
         const { animal } = this.props;
 
         try {
-            const response = await axios.post('http://127.0.0.1:8080/api/v1/applications/create', {
+            await axios.post('http://127.0.0.1:8080/api/v1/applications/create', {
                 shelter_id: animal.shelter_id,
                 user_id: user.userID,
                 animal_id: animal.id
             });
             await tryLoginUser();
-            console.log(response.data);
+            this.setState( { showAnimal: false } );
             alert('Заявку подано успішно!');
         } catch (error) {
-            console.error('Помилка при подачі заявки:', error.message);
             alert('Сталася помилка при подачі заявки.');
         }
     }
 
     render() {
+        const { showAnimal } = this.state;
         const { animal } = this.props;
         const { user } = this.context;
         const animalImage = animal.type === 'кіт' ? catImage : dogImage;
 
         return (
             <div>
-                <div className='animal-item'>
-                    <div>{animal.name}</div>
-                    <div className="animal-photo">
-                        <img src={animalImage} alt={'Фото ' + animal.type}/>
+                {showAnimal && (
+                    <div className='animal-item'>
+                        <div>{animal.name}</div>
+                        <div className="animal-photo">
+                            <img src={animalImage} alt={'Фото ' + animal.type}/>
+                        </div>
+                        <div className="grup-animal-info">
+                            <div>Вид тваринки: {animal.type}</div>
+                            <div>Стать: {animal.sex}</div>
+                            <div>Дата народження: {animal.month + "." + animal.year}</div>
+                            <div>Деталі: {animal.description}</div>
+                        </div>
+                        {user && (
+                            <button className="animal-info-button" onClick={this.handleSubmitApplication}>Подати заявку</button>
+                        )}
                     </div>
-                    <div className="grup-animal-info">
-                        <div>Вид тваринки: {animal.type}</div>
-                        <div>Стать: {animal.sex}</div>
-                        <div>Дата народження: {animal.month + "." + animal.year}</div>
-                        <div>Деталі: {animal.description}</div>
-                    </div>
-                    {user && (
-                        <button className="animal-info-button" onClick={this.handleSubmitApplication}>Подати заявку</button>
-                    )}
-                </div>
+                )}
             </div>
         );
     }
